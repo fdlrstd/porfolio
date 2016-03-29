e02f0f377b57690f5110b16f28307362b028f4e4
fdlrstd <jongofidel@yahoo.fr>
2016-03-29 12:27:25 +0200

    suite2

--- js/app.js
+++ js/app.js
@@ -3,13 +3,16 @@
 $(document).ready(function() {
 
     $('#fullpage').fullpage({
-
         anchors:['home','about', 'work','contact'],
-
         keyboardScrolling: false
-
     });
 
+    $('.slider').slick({ dots: true, arrows: false });
+
+
+	$(".btn-toggle-menu").click(function() {
+	    	$(".main-menu").toggleClass("hide-menu");
+	    })
+
 });
-
-
+		
